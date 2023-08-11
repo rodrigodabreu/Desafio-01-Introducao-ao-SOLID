d69c62924770b2038866fa63eab44ad35efa5590
Rodrigo Abreu <rodrigo.abreu@unicred.com.br>
2023-08-11 13:48:20 -0300

    chore: configurando o swagger ao projeto

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,15 @@
 import express from "express";
+import swaggertUi from "swagger-ui-express";
 
 import { usersRoutes } from "./routes/users.routes";
+import swaggerFile from "./swagger.json";
 
 const app = express();
 
 app.use(express.json());
 
 app.use("/users", usersRoutes);
+
+app.use("/api-docs", swaggertUi.serve, swaggertUi.setup(swaggerFile));
 
 export { app };
